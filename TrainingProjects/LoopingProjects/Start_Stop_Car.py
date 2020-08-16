8fb64da307e7578ead4727d65a211591319deca4
Nihal Waghmare <waghmare.nihal96@gmail.com>
2020-08-16 16:50:48 +0530

    Python Training Material

--- TrainingProjects/LoopingProjects/Start_Stop_Car.py
+++ TrainingProjects/LoopingProjects/Start_Stop_Car.py
@@ -1,4 +1,5 @@
 # Logic for executing Car commands.
+print("If you are running for first time, type 'help' to see commands")
 while 1:
     command = str.lower(input("> "))
     if command=="start":
@@ -8,6 +9,12 @@ while 1:
     elif command=="quit":
         print("Enjoy your day. Bye !!!")
         break
+    elif command=="help":
+        print("""
+        start   - To Start the Car.
+        stop    - To Stop the Car.
+        quit    - To Exit this program
+        """)
     else:
         print("Sorry I didn't get that !")
 
